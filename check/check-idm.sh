9f32f29aab32b1b321d9c4a4110a48741e86613f
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2022-02-03 02:32:37 +0800

    update idm

--- check/check-idm.sh
+++ check/check-idm.sh
@@ -6,9 +6,27 @@ export PATH
 DATE="$(echo $(TZ=UTC date '+%Y-%m-%d %H:%M:%S'))"
 tmpFile="./action.tmp"
 userAgent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-wget --user-agent="${userAgent}" -qO 'sha256sum.txt' 'https://pan.jiemi.workers.dev/?file=/Public/scoop/IDM/sha256sum.txt'
 
-localVersion="$(cat ../idm | grep -Po "IDM_v[\d.]+" | grep -Po "[\d.]+")"
-latestVersion="$(cat sha256sum.txt | grep -Po "IDM_v[\d.]+" | grep -Po "[\d.]+")"
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[Info]${Font_color_suffix}"
+Error="${Red_font_prefix}[Error]${Font_color_suffix}"
+Tip="${Green_font_prefix}[Tip]${Font_color_suffix}"
 
-mv -f sha256sum.txt ../idm
+idm_lrepacks_name="$(wget --user-agent="${userAgent}" --no-check-certificate -qO- "https://onedrive.xiazai.de/IDM/IDM-lrepacks%E7%A0%B4%E8%A7%A3%E7%89%88/" | grep -Po "internet.*.zip" | tail -n 1)"
+idm_lrepacks_ver="$(echo "${idm_lrepacks_name}" | grep -Po "[\d.]+\d")"
+
+latestVersion="${idm_lrepacks_ver}"
+localVersion=($(cat ../idm | jq -r ".version"))
+echo "Latest version: ${latestVersion}"
+echo "Local version: ${localVersion}"
+
+# Compare Version
+if [[ "${latestVersion}" == "${localVersion}" ]]; then
+    echo -e "${Green_font_prefix}[Info] IDM is the latest version!${Font_color_suffix}"
+else
+    echo -e "${Green_font_prefix}[Info] Update IDM!${Font_color_suffix}"
+    wget --user-agent="${userAgent}" --no-check-certificate -q "https://onedrive.xiazai.de/IDM/IDM-lrepacks%E7%A0%B4%E8%A7%A3%E7%89%88/${idm_lrepacks_name}"
+    hash="$(sha256sum ${idm_lrepacks_name} | awk '{print $1}')"
+    echo -e "{\"time\": \"${DATE}\",\"version\": \"${idm_lrepacks_ver}\",\"name\": \"${idm_lrepacks_name}\",\"hash\": \"${hash}\"}" | jq >../idm
+    rm -f *.zip
+fi
